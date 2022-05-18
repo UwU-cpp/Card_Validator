35749159e88d4011d1b24bf3e42674fe7838f1c0
UwU <shtykvladimir86@gmail.com>
2022-05-18 21:05:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Luhn Formula
+def validate(num_card):
+    nums = []
+    checker_num = int(num_card[len(num_card) - 1])
+
+    for i in range(len(num_card)-1):
+        nums.append(int(num_card[i]))
+
+    nums.reverse()
+
+    for i in range(0, len(nums), 2):
+        nums[i] *= 2
+
+    for i in range(len(nums)):
+        if nums[i] > 9:
+            nums[i] -= 9
+
+    check_sum = 0
+    for i in range(len(nums)):
+        check_sum += nums[i]
+    for i in range(len(nums)):
+        print(nums[i], end = ' ')
+    return (check_sum + checker_num) % 10 == 0
+if validate(input()):
+    print('It is valid')
+else:
+    print('It is invalid')
